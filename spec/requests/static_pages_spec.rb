8aae7ab2b40777700041aac95da61bf61da67698
Antoine Sakho <antoine.sakho@gmail.com>
2014-01-30 15:39:40 +0100

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -28,8 +28,7 @@ describe "Static Pages" do
 
   describe "Contact page" do
     before {visit contact_path}
-    it {should have_content('Contact Us')}
+    it {should have_selector('h1', text: 'Contact Us')}
     it {should have_title(full_title("Contact Us"))}
     end
   end
-
