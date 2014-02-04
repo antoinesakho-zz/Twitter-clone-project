773277f1d6aed6694c29daab716037ace99282f4
Antoine Sakho <antoine.sakho@gmail.com>
2014-02-04 22:03:49 +0100

    add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,8 @@ config.assets.compile = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+   # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
 end
