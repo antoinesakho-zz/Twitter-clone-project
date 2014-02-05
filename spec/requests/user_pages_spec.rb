a986e47832fe7a505669d953fafed473a9277bb8
Antoine Sakho <antoine.sakho@gmail.com>
2014-02-05 18:32:29 +0100

    add more tests and refactor signup page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,7 +21,45 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before {click_button submit}
+
+        it {should have_title("Sign up")}
+        it {should have_content("error")}
     end
+
+      describe "forget name" do
+        before do
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+        click_button submit
+      end
+      it {should have_content "Name can't be blank"}
+    end
+
+      describe "forget email" do
+        before do
+          fill_in "Name", with: "Example User"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+          click_button submit
+      end
+      it {should have_content("Email can't be blank")}
+    end   
+
+      describe "forget password" do
+        before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Confirmation", with: "foobar"
+        click_button submit
+      end
+      it {should have_content("Password can't be blank")}
+    end
+  end
+
 
     describe "with valid information" do
       before do
@@ -30,9 +68,15 @@ describe "User pages" do
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
-
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+      describe "after saving the user" do
+        before {click_button submit}
+        let(:user) {User.find_by(email:"user@example.com")}
+
+        it{should have_title(user.name)}
+        it{should have_selector("div.alert.alert-success", text:"Welcome to the Sample App!")}
       end
     end
   end
