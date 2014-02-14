fa2aaf4e74c4223d7bf197aa961aa2e71878d0d8
Antoine Sakho <antoine.sakho@gmail.com>
2014-02-14 16:27:07 +0100

    finish user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,10 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :microposts, dependent: :destroy
    
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
 
   def User.new_remember_token
   	SecureRandom.urlsafe_base64
